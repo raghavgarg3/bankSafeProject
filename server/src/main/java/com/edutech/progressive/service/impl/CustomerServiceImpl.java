590c52a2620f6301514406f264ec6e4006e6caec
raghavgarg3 <raghavgarg151515@gmail.com>
2026-02-02 20:43:15 +0530

    Update CustomerServiceImpl.java

--- server/src/main/java/com/edutech/progressive/service/impl/CustomerServiceImpl.java
+++ server/src/main/java/com/edutech/progressive/service/impl/CustomerServiceImpl.java
@@ -1,5 +1,58 @@
 package com.edutech.progressive.service.impl;
 
-public class CustomerServiceImpl {
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.edutech.progressive.dao.CustomerDAO;
+import com.edutech.progressive.dao.CustomerDAOImpl;
+import com.edutech.progressive.entity.Customers;
+import com.edutech.progressive.service.CustomerService;
+
+public class CustomerServiceImpl implements CustomerService  {
+
+    private CustomerDAO customerDAO;
+    
+    public CustomerServiceImpl(CustomerDAOImpl customerDAO){
+         this.customerDAO = customerDAO;
+    }
+
+    @Override
+    public List<Customers> getAllCustomers() throws SQLException {
+        // TODO Auto-generated method stub
+        // List<Customers> list = new ArrayList<>();
+        // return list;
+        return customerDAO.getAllCustomers();
+    }
+
+    @Override
+    public int addCustomer(Customers customers) throws SQLException {
+        // TODO Auto-generated method stub
+        // return -1;
+        // return customerDAO.addCustomer(customers);
+        int generatedId = customerDAO.addCustomer(customers);
+        customers.setCustomerId(generatedId);
+        return generatedId;
+    }
+
+    @Override
+    public List<Customers> getAllCustomersSortedByName() throws SQLException {
+        // TODO Auto-generated method stub
+        // List<Customers> list = new ArrayList<>();
+        // return list;
+        List<Customers> list = customerDAO.getAllCustomers();
+        list.sort((a,b) -> a.getName().compareToIgnoreCase(b.getName()));
+        return list;
+     }
+
+    public void updateCustomer(Customers customers) throws SQLException{
+            customerDAO.updateCustomer(customers);
+    }
+    public void deleteCustomer(int customerId)throws SQLException{
+           customerDAO.deleteCustomer(customerId);
+    }
+    public Customers getCustomerById(int customerId) throws SQLException{
+          return customerDAO.getCustomerById(customerId);
+    }
 
 }
