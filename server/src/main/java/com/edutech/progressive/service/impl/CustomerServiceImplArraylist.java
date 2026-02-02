b1bac731c347c7ad7c7d8a57ec3a6cc23aaae816
raghavgarg3 <raghavgarg151515@gmail.com>
2026-02-02 20:44:20 +0530

    Update CustomerServiceImplArraylist.java

--- server/src/main/java/com/edutech/progressive/service/impl/CustomerServiceImplArraylist.java
+++ server/src/main/java/com/edutech/progressive/service/impl/CustomerServiceImplArraylist.java
@@ -2,42 +2,30 @@ package com.edutech.progressive.service.impl;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 import com.edutech.progressive.entity.Customers;
-import com.edutech.progressive.service.CustomerService;
 
-public class CustomerServiceImplArraylist implements CustomerService {
-    private static final List<Customers> customersList = new ArrayList<>();
+public class CustomerServiceImplArraylist {
 
+    private static List<Customers> customersList = new ArrayList<>();
 
+    public List<Customers> getAllCustomers() throws SQLException {
+        return customersList;
+    }
 
-    @Override
     public int addCustomer(Customers customers) throws SQLException {
         customersList.add(customers);
         return customersList.size();
     }
 
-    @Override
-    public List<Customers> getAllCustomers() throws SQLException {
-       return customersList;
-    }
-
-    @Override
     public List<Customers> getAllCustomersSortedByName() throws SQLException {
-        // List<Customers> sorted = new ArrayList<>();
-        // Collections.sort(sorted);
-        // return sorted;
-        Collections.sort(customersList);
+        customersList.sort((a, b) ->
+                a.getName().compareToIgnoreCase(b.getName()));
         return customersList;
-       
     }
 
-    @Override
     public void emptyArrayList() {
         customersList.clear();
     }
-
 }
